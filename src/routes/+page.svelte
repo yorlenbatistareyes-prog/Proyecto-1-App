bcf94979c33ff3f119c40fd28e82b490f6989f0b
yorlenbatistareyes-prog <yorlenbatistareyes@gmail.com>
2025-12-12 18:15:48 -0500

    Pantalla principal inicial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+  <h1>Asistente de Visitas</h1>
+  <p>Organiza y controla todas tus visitas</p>
+
+  <div class="separador"></div>
+
+  <button>Registros recientes</button>
+  <button>Asuntos pendientes</button>
+</main>
+
+<style>
+  .separador {
+    margin-bottom: 20px;
+  }
+</style>
