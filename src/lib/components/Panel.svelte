f5e112bca2a3a27bb26fae88ab117f122f497abb
yorlenbatistareyes-prog <yorlenbatistareyes@gmail.com>
2025-12-13 13:38:52 -0500

    Agregar componente Panel

---
+++ src/lib/components/Panel.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let titulo = '';
+</script>
+
+<div class="panel">
+  {#if titulo}
+    <h2>{titulo}</h2>
+  {/if}
+
+  <slot />
+</div>
+
+<style>
+  .panel {
+    border: 1px solid #ccc;
+    padding: 1rem;
+    margin-top: 1rem;
+    border-radius: 6px;
+  }
+</style>
