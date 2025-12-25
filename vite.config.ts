5994dc761b822a7165fc6c798c75e119283dd940
yorlenbatistareyes-prog <yorlenbatistareyes@gmail.com>
2025-12-24 22:16:31 -0500

    App limpia para reiniciar proceso de Tauri más adelante

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    // Añadimos esta sección para que el puerto sea siempre el mismo
+    server: {
+        port: 5173,
+        strictPort: true, 
+    }
 });
